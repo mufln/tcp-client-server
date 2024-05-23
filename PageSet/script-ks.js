232ab40bd7c20c6774eba2ff74d29671ab7cbe75
peachonka <okneverak@gmail.com>
2024-05-23 12:43:06 +0300

    Ксюша добавила увеличение поля ввода сообщения

--- PageSet/script-ks.js
+++ PageSet/script-ks.js
@@ -1,22 +1,4 @@
-////////это не работает как надо
-var tx = document.getElementsByClassName('textarea');
-for (var i = 0; i < tx.length; i++) {
-  tx[i].setAttribute('style', 'height:' + (tx[i].scrollHeight) + 'px;overflow:auto;');
-  tx[i].addEventListener("input", OnInput, false);
-}
 
-function OnInput(e) {
-  this.style.height = 'auto';
-  this.style.height = (this.scrollHeight) + 'px';
-}
-
-//////////это тоже не работает
-
-function setfilename(val)
-  {
-    var fileName = val.substr(val.lastIndexOf("\\")+1, val.length);
-    document.getElementById("uploadFile").value = fileName;
-  }
 /////////////здесь мы автоматом крутим вниз
 var scrollDiv = document.getElementById("messages");
 scrollDiv.scrollTo(0, scrollDiv.scrollHeight);
